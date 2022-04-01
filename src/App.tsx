8c19d21beb4ae9b17b44e64d0731f0c0da662dca
Khalid Al-Awady <pipes_and_bottles@protonmail.com>
2022-04-01 16:50:36 -0700

    todo: styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { injectStyle } from 'react-toastify/dist/inject-style';
+import { ToastContainer } from 'react-toastify';
+
+if (typeof window !== 'undefined') {
+  injectStyle();
+}
 
 function App() {
   return (
     <div>
       <Outlet />
+      <ToastContainer />
     </div>
   );
 }
