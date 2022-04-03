2036b403a88c93c3bdb68bea4e64e19dbbb50dee
Khalid Al-Awady <pipes_and_bottles@protonmail.com>
2022-04-02 21:00:49 -0700

    adds todo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { injectStyle } from 'react-toastify/dist/inject-style';
 import WelcomPage from './shared/components/WelcomPage';
 
+// TODO: remove this and use proper CSS
 if (typeof window !== 'undefined') {
   injectStyle();
 }
