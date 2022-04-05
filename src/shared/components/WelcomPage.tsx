a6c72e549fa7d32fcc85e96f2c013ea13c86f0e4
Khalid Al-Awady <pipes_and_bottles@protonmail.com>
2022-04-04 18:03:43 -0700

    content fixes

--- src/shared/components/WelcomPage.tsx
+++ src/shared/components/WelcomPage.tsx
@@ -2,20 +2,24 @@ import { Link } from 'react-router-dom';
 
 export default function WelcomPage() {
   return (
-    <div className="row">
-      <div className="col">
-        <Link to='/signup' >
-          <button type='button' className='btn btn-dark'>
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-2">
+          <div className="col col-md-6 mt-3 ml-4">
+            <Link to='/signup' >
+              <button type='button' className='btn btn-dark'>
             Sign up
-          </button>
-        </Link>
-      </div>
-      <div className="col">
-        <Link to='/login' >
-          <button type='button' className='btn btn-dark'>
+              </button>
+            </Link>
+          </div>
+          <div className="col-md-6 mt-3">
+            <Link to='/login' >
+              <button type='button' className='btn btn-dark'>
             Login
-          </button>
-        </Link>
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
