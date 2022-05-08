40c773c0cfee4e75a4d35642ed2439e26da3f021
Khalid Al-Awady <pipes_and_bottles@protonmail.com>
2022-05-08 15:48:32 -0700

    form based

--- src/shared/components/WelcomPage.tsx
+++ src/shared/components/WelcomPage.tsx
@@ -1,32 +1,22 @@
-import { useReactMediaRecorder } from '../../MediaRecorder/MediaRecorder';
 import { useDispatch } from 'react-redux';
 import {
   errorNotice,
   successNotice } from '../redux/dialogs/actions/toastActions';
 import axios from 'axios';
+// import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function WelcomPage() {
+  const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
-  const { status, startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({
-      audio: true,
-      video: false,
-      blobPropertyBag: {
-        type: 'audio/wav',
-      },
-      mediaRecorderOptions: {
-        mimeType: 'audio/wav',
-      } });
+  // const [name, setName] = useState('');
+  // const [selectedFile, setSelectedFile] = useState<any>(null);
 
-  const onClick = async () => {
-    // @ts-ignore
-    const audioBlob = await fetch(mediaBlobUrl).then((r) => r.blob());
-    const audiofile = new File(
-      [audioBlob],
-      'audiofile.wav',
-      { type: 'audio/wav' });
+  const onClick = (data: any) => {
     const formData = new FormData();
-    formData.append('file', audiofile);
+    formData.append('file', data.file[0]);
+    // formData.append('file', selectedFile);
+    // formData.append('word', name);
     axios.post('http://0.0.0.0:8000/v1/arabic', formData)
       .then((response) => {
         dispatch(successNotice(response.data.msg));
@@ -41,35 +31,40 @@ export default function WelcomPage() {
   };
 
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col col-lg-2">
-          <div>
-            <p>{status}</p>
-            <button
-              className='btn btn-dark'
-              onClick={startRecording}
-            >
-              Start Recording
-            </button>
-            <button
-              className='btn btn-dark'
-              onClick={stopRecording}
-            >
-              Stop Recording
-            </button>
-            <audio src={ mediaBlobUrl } controls />
-          </div>
-          <div>
-            <button
-              type='submit'
-              className="btn btn-dark btn-large"
-              onClick={ onClick }>
-              submit
-            </button>
-          </div>
+    <form>
+      <div className="form-row">
+        <div className="form-group col-md-3">
+          <label htmlFor="input-email">word</label>
+          <input
+            type="text"
+            className="form-control"
+            id="input-word"
+            placeholder="Enter word"
+            {...register('word', { required: true })}
+          />
         </div>
       </div>
-    </div>
+      <div className="form-row">
+        <div className="form-group col-md-3 mt-3">
+          <label htmlFor="input-password">File</label>
+          <input
+            type="file"
+            className="form-control"
+            id="input-file"
+            {...register('file', { required: true })}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group col-md-3 mt-3">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit(onClick)}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
